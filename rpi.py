2a0bced21f87a6876b4c82e320a72a9af8af6f9d
Mate Varga <47510515+03vmate@users.noreply.github.com>
2020-12-17 22:05:42 +0100

    Update rpi.py

--- rpi.py
+++ rpi.py
@@ -4,17 +4,38 @@ import tensorflow as tf
 from tensorflow.keras import layers
 from tensorflow.keras import regularizers
 from threading import Thread
+import mysql.connector
+import time
+import math
+
+db = mysql.connector.connect(host="127.0.0.1", user="pi", password="qwerty", database="DrinkAI")
 
 model = tf.keras.models.load_model('model/ai')
 
+lastTimestamp = 0
+
 def process(data):
+    global lastTimestamp
+    global db
+    unixtime = int(time.time())
     data = data / (np.max(data) if np.max(data) > abs(np.min(data)) else abs(np.min(data)))
     pred = model.predict(np.expand_dims(data,0))
-    if(pred[0][0] > pred[0][1]):
-        print("Not drinking")
-    else:
-        print("Drinking")
-    print(pred)
+
+    score = pred[0][1]-pred[0][0]
+    if(not math.isnan(score)):
+        try:
+            c = db.cursor()
+            if(lastTimestamp != 0):
+                if(unixtime - lastTimestamp > 1):
+                    for i in range(int((unixtime - lastTimestamp) - 1)):
+                        c.execute("INSERT INTO Drink (score, timestamp) VALUES (%s, %s)", (float(score), lastTimestamp + i + 1))
+            c.execute("INSERT INTO Drink (score, timestamp) VALUES (%s, %s)", (float(score), unixtime))
+            lastTimestamp = unixtime
+            db.commit()
+        except Exception as e:
+            print(e)
+            db = mysql.connector.connect(host="127.0.0.1", user="pi", password="qwerty", database="DrinkAI")
+
 
 thr = None
 try:
